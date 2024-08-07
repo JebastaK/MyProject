b936c9cd8ce59a902c3c385d42c00014184dbf28
jebastaR <jebastarakesh@gmail.com>
2024-08-07 09:47:57 +0530

    MyProj

---
+++ src/org/fb/Facebook.java
@@ -0,0 +1,28 @@
+package org.fb;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Facebook {
+	public static void main(String[] args) {
+
+		//String reverse
+		//String Char Occ & word occ
+		//String palindrome
+		//Get the uppercase,lowercase,numbers, special
+
+		String s = "Javaaaeviuba";
+		
+		
+		String s1 = "I have two pen and two pencil";
+		
+		//I=1, have =1 two=2, pen =1 and =1, pencil =1
+		
+		//Character count = but remove empty space
+		
+
+
+
+	}
+
+}
